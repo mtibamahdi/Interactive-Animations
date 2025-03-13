71d10ce756b2e231b7f3e8a9a64f136e1b363f29
mtibamahdi <mtiba.mahdi@gmail.com>
2025-03-13 02:25:46 +0100

    Bsic Setup

---
+++ script.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.fillStyle = "white";
+ctx.fillRect(10, 20, 50, 150);
