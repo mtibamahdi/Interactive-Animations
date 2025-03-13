4b5b84891b480cadb3ba3a90bb541517d82389b4
mtibamahdi <mtiba.mahdi@gmail.com>
2025-03-13 02:34:29 +0100

    Resize EventListener

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.fillStyle = "white";
+  ctx.fillRect(10, 20, 150, 50);
+});
 
 ctx.fillStyle = "white";
-ctx.fillRect(10, 20, 50, 150);
+ctx.fillRect(10, 20, 150, 50);
