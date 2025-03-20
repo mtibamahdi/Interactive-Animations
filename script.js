568baca8c7feb3d228c66dd994636df5b6319ac8
mtibamahdi <mtiba.mahdi@gmail.com>
2025-03-20 18:10:01 +0100

    Drawing a Circle

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const canvas = document.getElementById("canvas1");
+/** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d");
 
 window.addEventListener("resize", function () {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  ctx.fillStyle = "white";
-  ctx.fillRect(10, 20, 150, 50);
 });
 
-ctx.fillStyle = "white";
-ctx.fillRect(10, 20, 150, 50);
+ctx.fillStyle = "blue";
+ctx.beginPath();
+ctx.arc(100, 100, 50, 0, Math.PI * 2);
+ctx.fill();
