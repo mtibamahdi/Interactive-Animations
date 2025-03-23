25cfd1f94da11977d00fdee1352e2489f94154f9
mtibamahdi <mtiba.mahdi@gmail.com>
2025-03-23 08:14:14 +0100

    HSL Colors

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const canvas = document.getElementById("canvas1");
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d");
-const particlesArray = [];
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const particlesArray = [];
+let hue = 0;
 
 window.addEventListener("resize", function () {
   canvas.width = window.innerWidth;
@@ -39,6 +39,7 @@ class Particle {
     this.size = Math.random() * 16;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
+    this.color = `hsl(${hue},100%,50%)`;
   }
   update() {
     this.x += this.speedX;
@@ -48,7 +49,7 @@ class Particle {
     }
   }
   draw() {
-    ctx.fillStyle = "white";
+    ctx.fillStyle = this.color;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
@@ -90,10 +91,11 @@ function handleParticles() {
 }
 
 function animate() {
-  //ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "rgba(0, 0, 0, 0.2)";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // ctx.fillStyle = "rgba(0, 0, 0, 0.02)";
+  // ctx.fillRect(0, 0, canvas.width, canvas.height);
   handleParticles();
+  hue += 2;
   requestAnimationFrame(animate);
 }
 
